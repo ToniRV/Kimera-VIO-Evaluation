75b4a840ecbd1f834035ed98f6e052d0a16ed919
Andrew Violette <aviolett@mit.edu>
2020-06-16 17:49:57 -0400

    Removed old/useless/incomplete scripts

--- scripts/extract_tf_odemetry.py
+++
@@ -1,8 +0,0 @@
-import rosbag
-import re
-print '#timestamp,x,y,z,qw,qx,qy,qz,vx,vy,vz,bgx,bgy,bgz,bax,bay,baz'
-for topic, msg, t in rosbag.Bag('tressider-2019-04-26_0.bag').read_messages():
-    if re.search("tf", topic):
-	for transform in msg.transforms:
-	    if(transform.header.frame_id=="odom" and transform.child_frame_id=="base_link"):
-		print '{}, {}, {}, {}, {}, {}, {}, {},,,,,,,,,'.format(transform.header.stamp, transform.transform.translation.x, transform.transform.translation.y, transform.transform.translation.z, transform.transform.rotation.w, transform.transform.rotation.x, transform.transform.rotation.y, transform.transform.rotation.z)
